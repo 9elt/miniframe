b2be583b178c5e2fe9ac2c2a2ff5fd9298e2dcf8
9elt <nelt.primary@gmail.com>
2024-01-23 21:45:40 +0100

    types tests

--- test.ts
+++ test.ts
@@ -1,12 +1,33 @@
 import { MiniframeElement, createNode } from "./index";
 
-let a: MiniframeElement<"http://www.w3.org/2000/svg", "svg"> = {
+const divTest: MiniframeElement = {
+    tagName: 'div',
+    className: 'class',
+    children: [{
+        tagName: 'p',
+        children: [
+            '0',
+            0,
+            null,
+            false,
+            undefined,
+            document.createElement('span'),
+        ],
+    }],
+};
+
+createNode(divTest);
+
+const svgTest: MiniframeElement<"http://www.w3.org/2000/svg", "svg"> = {
     tagName: 'svg',
     namespaceURI: "http://www.w3.org/2000/svg",
     style: {},
     viewBox: '0 0 64 64',
-    children: []
+    children: [{
+        tagName: "path",
+        namespaceURI: "http://www.w3.org/2000/svg",
+        d: "M0,0 64,0 64,64 0,64z",
+    }]
 };
 
-createNode(a);
-
+createNode(svgTest);
